0703f8b43770aad8c4ef20717d3ef4ddb906f97d
ahmadsaad007 <ahmadsd97@gmail.com>
2020-09-18 16:55:13 -0400

    App Works completely

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-app';
   inputValue:string = '';
-  counter:number = 0;
-  blueText:string = '';
+  count = 0;
+  counter: Array<number> = [];
+  // blueText:string = '';
+  isButtonVisible = false;
 
   onIncreaseClick(){
-    this.counter++;
-    if(this.counter==5){
-      this.blueText = 'blue';
-    }
+    this.counter.push(this.count++);
+    this.isButtonVisible = !this.isButtonVisible;
+    // if(this.count==5){
+    //   this.blueText = 'blue';
+    // }
     console.log('Increase Button Click');
   }
-  onClick(){
-    //this.inputValue = '';
+  // onClick(){
+  //   //this.inputValue = '';
+  // }
+  getColor(number){
+    if(number>4){
+      return 'blue';
+    }
+    else{
+      return 'white';
+    }
+  }
+  getText(num){
+    if(num>4){
+      return 'white';
+    }
+    else{
+      return 'black';
+    }
   }
 }
